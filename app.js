69c6f8d7e05525f5adecd3796cb95d9775b08c30
Chris Ota <chrisota@highgrovecrmdesign.com>
2026-09-25 11:34:14 -0600

    Final Updates

--- app.js
+++ app.js
@@ -2,4 +2,16 @@ import express from "express";
 const app = express();
 export default app;
 
-// TODO: this file!
+import employeesRouter from `#api/employees`;
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the Fullstack Employees API")
+});
+
+
+app.use("/employees",employeesRouter);
+
+
+app.use((err, req, res, next) => {
+  res.status(500).send("Sorry, something went wrong :(");
+});
--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,13 +1,30 @@
 /** @returns the employee created according to the provided details */
+import db from "#db/client";
+
 export async function createEmployee({ name, birthday, salary }) {
-  // TODO
+  const sql = `
+  INSERT INTO employees
+    (name, birthday, salary)
+  VALUES
+    ($1, $2, $3)
+  Returning *
+  `;
+
+  const {
+    rows : [employee],
+  } = await db.query(sql, [name, birthday, salary]);
+  return employee;
 }
 
 // === Part 2 ===
 
 /** @returns all employees */
 export async function getEmployees() {
-  // TODO
+  const sql = 
+  `SELECT * FROM employees;`
+
+  const {rows: employees} = await db.query(sql);
+  return employees;
 }
 
 /**
@@ -15,7 +32,12 @@ export async function getEmployees() {
  * @returns undefined if employee with the given id does not exist
  */
 export async function getEmployee(id) {
-  // TODO
+  const sql = `
+  SELECT * FROM employees
+  WHERE id = $1;
+  `
+  const { rows : [employee]} = await db.query(sql,[id]);
+  return employee;
 }
 
 /**
@@ -23,7 +45,19 @@ export async function getEmployee(id) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
+  const sql = `
+    UPDATE employees 
+    SET 
+      name = $2,
+      birthday=$3,
+      salary=$4
+      WHERE id = $1
+
+    Returning *
+  `;
+
+  const { rows: [employee] } = await db.query(sql, [id, name, birthday, salary]);
+  return employee;
 }
 
 /**
@@ -31,5 +65,16 @@ export async function updateEmployee({ id, name, birthday, salary }) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function deleteEmployee(id) {
-  // TODO
+  const sql = `
+  DELETE FROM employees
+  WHERE id = $1
+
+  Returning *
+  `;
+
+  const {
+    rows: [employee],
+  } = await db.query(sql, [id]);
+  return employee;
 }
+
